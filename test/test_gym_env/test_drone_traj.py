e3e5fb6b909829058b63effed00853907c75fc64
Hany Hamed <h.hamed.elanwar@gmail.com>
2021-06-21 00:00:37 +0300

    [Test] add some tests for environments

--- test/test_gym_env/test_drone_traj.py
+++ test/test_gym_env/test_drone_traj.py
@@ -52,6 +52,11 @@ def plot_traj(traj_func):
 point_coord = [0,0.5,0.5]
 
 # Source: https://github.com/caelan/pybullet-planning/blob/6af327ba03eb32c0c174656cca524599c076e754/pybullet_tools/utils.py#L4415
+def add_line(start, end, color=[0,0,0], width=1, lifetime=None, parent=-1, parent_link=-1):
+    assert (len(start) == 3) and (len(end) == 3)
+    return p.addUserDebugLine(start, end, lineColorRGB=color[:3], lineWidth=width,
+                              parentObjectUniqueId=parent, parentLinkIndex=parent_link)
+                              
 def draw_point(point, size=0.01, **kwargs):
     lines = []
     for i in range(len(point)):
@@ -62,10 +67,6 @@ def draw_point(point, size=0.01, **kwargs):
         lines.append(add_line(p1, p2, **kwargs))
     return lines
 
-def add_line(start, end, color=[0,0,0], width=1, lifetime=None, parent=-1, parent_link=-1):
-    assert (len(start) == 3) and (len(end) == 3)
-    return p.addUserDebugLine(start, end, lineColorRGB=color[:3], lineWidth=width,
-                              parentObjectUniqueId=parent, parentLinkIndex=parent_link)
 
 if __name__ == "__main__":
 
